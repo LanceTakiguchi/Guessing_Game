c0fad7294a3d78624894bbb33831f73c1c39001f
Lance Takiguchi <lancetakiguchi@gmail.com>
2017-06-15 11:09:22 -0700

    isSame comparing function completed.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 var aINumGen = function(start, end){
 	return Math.floor((end - start + 1) * Math.random()) + start;
 }
+var isSame = function(num1, num2){
+	return num1 === num2;
+}
 $(document).ready(function() {
 	$(".submitPick").click(function(){
-		var userGuess = $(".userGuess").val();
+		var userGuess = parseInt($(".userGuess").val());
 		var aIPick = aINumGen(1,10);
+		var result = isSame(userGuess, aIPick);
+		console.log(result);
 	});
 });
