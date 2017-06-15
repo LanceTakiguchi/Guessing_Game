d7f1cae4c376fd1f18726b8ba778ae85fef55e9f
Lance Takiguchi <lancetakiguchi@gmail.com>
2017-06-15 11:29:38 -0700

    Console logs removed. JSDocs added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+/**
+ * aINumGen
+ * @param  {[Int]} A integer that is the start of the range, inclusive
+ * @param  {[Int]} A integer that is the end of the range, inclusive
+ * @return {[Int]} A random number between the start and finish
+ */
 var aINumGen = function(start, end){
 	return Math.floor((end - start + 1) * Math.random()) + start;
 }
+/**
+ * isSame
+ * @param  {[Int]} User inputed integer that is their guess
+ * @param  {[Int]} AI generated integer
+ * @return {Boolean} Determines if the two numbers where the same
+ */
 var isSame = function(num1, num2){
 	return num1 === num2;
 }
@@ -9,6 +21,5 @@ $(document).ready(function() {
 		var userGuess = parseInt($(".userGuess").val());
 		var aIPick = aINumGen(1,10);
 		var result = isSame(userGuess, aIPick);
-		console.log(result);
 	});
 });
